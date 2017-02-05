4a9a57398a4a974668ee1e4f3dda70e53ee99b08
Jacob Radcliffe <jradcli2@my.westga.edu>
2017-02-05 12:23:21 -0600

    Initial commit.
    MVC folders set up.
    
    DAL only has a connection string class. Will need to add in "InitialCatalog" once we know our official DB name.

--- MedTracker/Controller/Driver.cs
+++ MedTracker/Controller/Driver.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace MedTracker
 {
-    static class Program
+    static class Driver
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace MedTracker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
---
+++ MedTracker/DBA/DBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedTracker.DBA
+{
+    class DBConnection
+    {
+
+        /// <summary>
+        /// Method to build an SqlConnection to allow access to our DB.
+        /// </summary>
+        /// <returns>SqlConnection to TechSupport DB</returns>
+        public static SqlConnection GetConnection()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "localhost";
+            builder.InitialCatalog = "";
+            builder.IntegratedSecurity = true;
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            return connection;
+        }
+
+    }
+}

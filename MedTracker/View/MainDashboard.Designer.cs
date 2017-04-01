1088ff9df5aa5166251808f112a4887138d4f492
Troy Hilley <thilley1@my.westga.edu>
2017-04-01 07:16:35 -0400

    Added labels for user information to appear on Main Dashbaord

--- MedTracker/View/MainDashboard.Designer.cs
+++ MedTracker/View/MainDashboard.Designer.cs
@@ -32,6 +32,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.lblInformationUsername = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblRoleInformation = new System.Windows.Forms.Label();
+            this.lblRole = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblUsername
@@ -39,9 +42,9 @@
             this.lblUsername.AutoSize = true;
             this.lblUsername.Location = new System.Drawing.Point(243, 32);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(70, 25);
+            this.lblUsername.Size = new System.Drawing.Size(260, 25);
             this.lblUsername.TabIndex = 0;
-            this.lblUsername.Text = "label1";
+            this.lblUsername.Text = "UsernameWillAppearHere";
             // 
             // button1
             // 
@@ -65,17 +68,47 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(170, 64);
+            this.label2.Location = new System.Drawing.Point(170, 67);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Name:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(244, 67);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(211, 25);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "NameOfPersonLogin";
+            // 
+            // lblRoleInformation
+            // 
+            this.lblRoleInformation.AutoSize = true;
+            this.lblRoleInformation.Location = new System.Drawing.Point(182, 101);
+            this.lblRoleInformation.Name = "lblRoleInformation";
+            this.lblRoleInformation.Size = new System.Drawing.Size(62, 25);
+            this.lblRoleInformation.TabIndex = 5;
+            this.lblRoleInformation.Text = "Role:";
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Location = new System.Drawing.Point(243, 101);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(275, 25);
+            this.lblRole.TabIndex = 6;
+            this.lblRole.Text = "RoleOfTheUserAppearHere";
             // 
             // MainDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1330, 807);
+            this.Controls.Add(this.lblRole);
+            this.Controls.Add(this.lblRoleInformation);
+            this.Controls.Add(this.lblName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblInformationUsername);
             this.Controls.Add(this.button1);
@@ -95,5 +128,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lblInformationUsername;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblRoleInformation;
+        private System.Windows.Forms.Label lblRole;
     }
 }
